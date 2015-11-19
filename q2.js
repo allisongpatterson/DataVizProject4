cc99c6284d56cc83d132c634a8beb49ea3bbe59e
Karina <kchan4@wellesley.edu>
2015-11-19 15:12:16 -0500

    one line on graph, need to fix the sum total function

--- q2.js
+++ q2.js
@@ -1,192 +1,234 @@
-
-
 /*
- * Demo: Mortality 2008/2013
- * 
- */
-
-
-
-
-
-
+* Demo: Mortality 2008/2013
+*
+*/
 // Call run when the page finishes loading
-      
+
 window.addEventListener("load",run);
-
-
-var GLOBAL = {};
-
+var GLOBAL={"data":"",
+"causes":CAUSE,
+"months":[1,2,3,4,5,6,7,8,9,10,11,12],
+"years":[2003, 2008, 2013],
+"selected_causes":[],
+"totals":[]};
 function run () {
-    var svg = d3.selectAll("svg");
-    svg.append("text")
-	.attr("class","loading")
-	.attr("x",+svg.attr("width")/2)
-	.attr("y",+svg.attr("height")/2)
-	.attr("dy","0.35em")
-	.style("text-anchor","middle")
-	.text("loading data...");
+var svg = d3.selectAll("svg");
+svg.append("text")
+.attr("class","loading")
+.attr("x",+svg.attr("width")/2)
+.attr("y",+svg.attr("height")/2)
+.attr("dy","0.35em")
+.style("text-anchor","middle")
+.text("loading data...");
+getData(function(data) {
+GLOBAL.data = data["data"];
+GLOBAL.cause=CAUSE;
+GLOBAL.months = data["month"];
+GLOBAL.years = data["year"];
+updateCausesView();
+});
+}
+/*
+* Summing up totals from the causes. Run the checkChanged before running sum_Total_Months.
+*/
+function sum_Total_Months()
+{
 
-    getData(function(data) {
-	GLOBAL.data = data;
-	createAgesView(data);
-    });
+GLOBAL.data.forEach(function(r) //for each row in the data
+{
+
+if (GLOBAL.selected_causes.length==0){ //take it as taking the totals of all causes
+if (r.month in GLOBAL.totals){ //if it is, just update
+
+GLOBAL.totals[r["month"]]={"month":r["month"],"number":GLOBAL.totals[r["month"]]["number"]+r["number"]}
+
+} else { //if that month isn't in the total
+GLOBAL.totals[r["month"]]={"month":r["month"],"number":r["number"]}; // month: number
+
 }
 
+}
+// If the country of the data row is the desired country
+else if (GLOBAL.selected_causes.indexOf(r.cause)>-1) //if it is in selected countries
+{
+if (!GLOBAL.totals.indexOf(r["month"])){ //if that month isn't in the total
+GLOBAL.totals[r["month"]]=r["number"];
+} else { //if it is, just update
+GLOBAL.totals[r["month"]]=GLOBAL.totals[r["month"]]+r["number"];
+}
+}
+});
+return GLOBAL.totals
+}
 
+/* Checks whenever a checkbox has been clicked
+* and updates the viz accordingly to add or remove the cause from the totals
+*/
+function check_changed()
+{
+
+    var svg = d3.select("#viz-age");
+svg.selectAll("*").remove(); //clears the viz
+// Reset the list of selected countries
+
+GLOBAL.totals=[];
+
+// Fill the list up with all countries that are checked
+for (cause_index in GLOBAL.causes)
+{
+var cause = GLOBAL.causes[cause_index]
+if (document.getElementById(cause_index).checked === true)
+{
+GLOBAL.selected_causes.push(cause);
+}
+}
+// Update the visualization
+updateCausesView();
+}
+function updateCausesView(){
+createCausesView(sum_Total_Months());
+}
 /*
- * Functions for summing up totals from the
- * data
- * 
- */
+* Create a simple visual representation
+* of the data
+*
+*/
+function createCausesView() {
+var svg = d3.select("#viz-age");
+svg.append("text")
+.attr("x",+svg.attr("width")/2)
+.attr("y",20)
+.attr("dy","0.35em")
+.style("text-anchor","middle")
+.style("font-family","sans-serif")
+.style("font-size","20pt")
+.text("Causes of Death");
 
-function createAgesView(data){
-var margin = {
-    top: 50,
-    right: 50,
-    bottom: 50,
-    left: 50
+var vis = svg,
+WIDTH = 1000,
+HEIGHT = 500,
+MARGINS = {
+top: 20,
+right: 20,
+bottom: 20,
+left: 50
 },
-width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([1, 24]),
+yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([100000, 250000]),
+xAxis = d3.svg.axis()
+.scale(xScale),
+yAxis = d3.svg.axis()
+.scale(yScale)
+.orient("left");
+
+console.log("Reached");
 
 
-
-//get the max y of the domain, so that itll never go beyond screen
-var sum = new Array(data.length); //placeholder array
-for(var x=0; x<data.length; x++) {
-    sum[x] = 0;
-    for(var y=0; y<data.length; y++) {
-        sum[x] += data[y][x];   //sum up values vertically
-    }
-}
-
-// permute the data 
-
-data = data["data"].forEach(function (d) {
-	console.log(d);
-    return d['test']=(function (p, i) {
-        return {
-            x: i,
-            y: p,
-            y0: 0
-        };
-    });
+var line = d3.svg.line()
+.x(function(d) {
+return xScale(d.month);
+})
+.y(function(d) {
+console.log(d.number);
+return yScale(d.number);
 });
 
-var color = d3.scale.linear()
-    .range(["#0A3430", "#1E5846", "#3E7E56", "#6BA55F", "#A4CA64", "#E8ED69"]);
-
-var x = d3.scale.linear()
-    .range([0, width])
-    .domain([0, 130]); //the ages?
-
-var y = d3.scale.linear()
-    .range([height, 0])
-    .domain([0, d3.max(sum)]); //max y is the sum we calculated earlier
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var stack = d3.layout.stack()
-    .offset("wiggle") //<-- creates a streamgraph
-
-var layers = stack(data);
-
-//vis type
-var area = d3.svg.area()
-    .interpolate('cardinal')
-    .x(function (d, i) {
-    return x(i);
+var simpler=[]; //the other array of GLOBALS.total had the month in it for easier updating...
+GLOBAL.totals.forEach(function(r){
+simpler.push(r); //we'll just use this :')
 })
-    .y0(function (d) {
-    return y(d.y0);
-})
-    .y1(function (d) {
-    return y(d.y0 + d.y);
-});
-
-svg.selectAll(".layer")
-    .data(layers)
-    .enter().append("path")
-    .attr("class", "layer")
-    .attr("d", function (d) {
-    return area(d);
-})
-    .style("fill", function (d, i) {
-    return color(i);
-});
+console.log(simpler);
+vis.append("svg:g")
+.attr("class", "x axis")
+.attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+.call(xAxis);
+vis.append("svg:g")
+.attr("class", "y axis")
+.attr("transform", "translate(" + (MARGINS.left) + ",0)")
+.call(yAxis);
+vis.append("svg:path").attr("d", line(simpler));
 
 }
-
-
-
-var CAUSE = {
-    "1":"Tuberculosis (A16-A19)",
-    "2":"Syphilis (A50-A53)",
-    "3":"Human immunodeficiency virus (HIV) disease (B20-B24)",
-    "4":"Malignant neoplasms (C00-C97)",
-    "5":"Malignant neoplasm of stomach (C16)",
-    "6":"Malignant neoplasms of colon, rectum and anus (C18-C21)",
-    "7":"Malignant neoplasm of pancreas (C25)",
-    "8":"Malignant neoplasms of trachea, bronchus and lung (C33-C34)",
-    "9":"Malignant neoplasm of breast (C50)",
-    "10":"Malignant neoplasms of cervix uteri, corpus uteri and ovary (C53-C56)",
-    "11":"Malignant neoplasm of prostate (C61)",
-    "12":"Malignant neoplasms of urinary tract (C64-C68)",
-    "13":"Non-Hodgkin's lymphoma (C82-C85)",
-    "14":"Leukemia (C91-C95)",
-    "15":"Other malignant neoplasms (C00-C15,C17,C22-C24,C26-C32,C37-C49,C51-C52, C57-C60,C62-C63,C69-C81,C88,C90,C96-C97)",
-    "16":"Diabetes mellitus (E10-E14)",
-    "17":"Alzheimer's disease (G30)",
-    "18":"Major cardiovascular diseases (I00-I78)",
-    "19":"Diseases of heart (I00-I09,I11,I13,I20-I51)",
-    "20":"Hypertensive heart disease with or without renal disease (I11,I13)",
-    "21":"Ischemic heart diseases (I20-I25)",
-    "22":"Other diseases of heart (I00-I09,I26-I51)",
-    "23":"Essential (primary) hypertension and hypertensive renal disease (I10,I12,I15)",
-    "24":"Cerebrovascular diseases (I60-I69)",
-    "25":"Atherosclerosis (I70)",
-    "26":"Other diseases of circulatory system (I71-I78)",
-    "27":"Influenza and pneumonia (J09-J18)",
-    "28":"Chronic lower respiratory diseases (J40-J47)",
-    "29":"Peptic ulcer (K25-K28)",
-    "30":"Chronic liver disease and cirrhosis (K70,K73-K74)",
-    "31":"Nephritis, nephrotic syndrome, and nephrosis (N00-N07,N17-N19,N25-N27)",
-    "32":"Pregnancy, childbirth and the puerperium (O00-O99)",
-    "33":"Certain conditions originating in the perinatal period (P00-P96)",
-    "34":"Congenital malformations, deformations and chromosomal abnormalities (Q00-Q99)",
-    "35":"Sudden infant death syndrome (R95)",
-    "36":"Symptoms, signs and abnormal clinical and laboratory findings, not  elsewhere classified (excluding Sudden infant death syndrome) (R00-R94,R96-R99)",
-    "37":"All other diseases (Residual) (A00-A09,A20-A49,A54-B19,B25-B99,D00-E07, E15-G25,G31-H93,I80-J06,J20-J39,J60-K22,K29-K66,K71-K72, K75-M99,N10-N15,N20-N23,N28-N98,U04)",
-    "38":"Motor vehicle accidents (V02-V04,V09.0,V12-V14,V19.0-V19.2,V19.4-V19.6, V20-V79,V80.3-V80.5,V81.0-V81.1,V82.0-V82.1,V83-V86,V87.0-V87.8, V88.0-V88.8,V89.0,V89.2)",
-    "39":"All other and unspecified accidents and adverse effects (V01,V05-V06,V09.1,V09.3-V09.9,V10-V11,V15-V18,V19.3,V19.8-V19.9, V80.0-V80.2,V80.6-V80.9,V81.2-V81.9,V82.2-V82.9,V87.9,V88.9,V89.1, V89.3,V89.9,V90-X59,Y40-Y86,Y88)",
-    "40":"Intentional self-harm (suicide) (*U03,X60-X84,Y87.0)",
-    "41":"Assault (homicide) (*U01-*U02,X85-Y09,Y87.1)",
-    "42":"All other external causes (Y10-Y36,Y87.2,Y89)"
+/*
+* Move a DOM element to the front
+*
+*/
+function moveToFront (node) {
+var parent = node.parentNode;
+parent.appendChild(node);
 }
-    
 
 /*
- * Pulls the data
- * 
- */
-
-function getData (f) {
-
-    d3.json("data",function(error,data) {
-		 if (error) {
-		     d3.selectAll(".loading").remove();
-		     console.log(error);
-		 } else {
-		     d3.selectAll(".loading").remove();
-		  
-		     console.log(" data =", data);
-		     f(data);
-		 }
-	     });
+* Convert a cause code to text
+*
+*/
+function causeText (i) {
+var index = CAUSE[i].indexOf("(");
+if (index < 0) {
+return CAUSE[i];
+} else {
+return CAUSE[i].slice(0,index-1);
 }
-      
+}
+var CAUSE = {
+"1":"Tuberculosis (A16-A19)",
+"2":"Syphilis (A50-A53)",
+"3":"Human immunodeficiency virus (HIV) disease (B20-B24)",
+"4":"Malignant neoplasms (C00-C97)",
+"5":"Malignant neoplasm of stomach (C16)",
+"6":"Malignant neoplasms of colon, rectum and anus (C18-C21)",
+"7":"Malignant neoplasm of pancreas (C25)",
+"8":"Malignant neoplasms of trachea, bronchus and lung (C33-C34)",
+"9":"Malignant neoplasm of breast (C50)",
+"10":"Malignant neoplasms of cervix uteri, corpus uteri and ovary (C53-C56)",
+"11":"Malignant neoplasm of prostate (C61)",
+"12":"Malignant neoplasms of urinary tract (C64-C68)",
+"13":"Non-Hodgkin's lymphoma (C82-C85)",
+"14":"Leukemia (C91-C95)",
+"15":"Other malignant neoplasms (C00-C15,C17,C22-C24,C26-C32,C37-C49,C51-C52, C57-C60,C62-C63,C69-C81,C88,C90,C96-C97)",
+"16":"Diabetes mellitus (E10-E14)",
+"17":"Alzheimer's disease (G30)",
+"18":"Major cardiovascular diseases (I00-I78)",
+"19":"Diseases of heart (I00-I09,I11,I13,I20-I51)",
+"20":"Hypertensive heart disease with or without renal disease (I11,I13)",
+"21":"Ischemic heart diseases (I20-I25)",
+"22":"Other diseases of heart (I00-I09,I26-I51)",
+"23":"Essential (primary) hypertension and hypertensive renal disease (I10,I12,I15)",
+"24":"Cerebrovascular diseases (I60-I69)",
+"25":"Atherosclerosis (I70)",
+"26":"Other diseases of circulatory system (I71-I78)",
+"27":"Influenza and pneumonia (J09-J18)",
+"28":"Chronic lower respiratory diseases (J40-J47)",
+"29":"Peptic ulcer (K25-K28)",
+"30":"Chronic liver disease and cirrhosis (K70,K73-K74)",
+"31":"Nephritis, nephrotic syndrome, and nephrosis (N00-N07,N17-N19,N25-N27)",
+"32":"Pregnancy, childbirth and the puerperium (O00-O99)",
+"33":"Certain conditions originating in the perinatal period (P00-P96)",
+"34":"Congenital malformations, deformations and chromosomal abnormalities (Q00-Q99)",
+"35":"Sudden infant death syndrome (R95)",
+"36":"Symptoms, signs and abnormal clinical and laboratory findings, not  elsewhere classified (excluding Sudden infant death syndrome) (R00-R94,R96-R99)",
+"37":"All other diseases (Residual) (A00-A09,A20-A49,A54-B19,B25-B99,D00-E07, E15-G25,G31-H93,I80-J06,J20-J39,J60-K22,K29-K66,K71-K72, K75-M99,N10-N15,N20-N23,N28-N98,U04)",
+"38":"Motor vehicle accidents (V02-V04,V09.0,V12-V14,V19.0-V19.2,V19.4-V19.6, V20-V79,V80.3-V80.5,V81.0-V81.1,V82.0-V82.1,V83-V86,V87.0-V87.8, V88.0-V88.8,V89.0,V89.2)",
+"39":"All other and unspecified accidents and adverse effects (V01,V05-V06,V09.1,V09.3-V09.9,V10-V11,V15-V18,V19.3,V19.8-V19.9, V80.0-V80.2,V80.6-V80.9,V81.2-V81.9,V82.2-V82.9,V87.9,V88.9,V89.1, V89.3,V89.9,V90-X59,Y40-Y86,Y88)",
+"40":"Intentional self-harm (suicide) (*U03,X60-X84,Y87.0)",
+"41":"Assault (homicide) (*U01-*U02,X85-Y09,Y87.1)",
+"42":"All other external causes (Y10-Y36,Y87.2,Y89)"
+}
+
+/*
+* Pulls the data
+*
+*/
+function getData (f) {
+d3.json("data",function(error,data) {
+if (error) {
+d3.selectAll(".loading").remove();
+console.log(error);
+} else {
+d3.selectAll(".loading").remove();
+
+console.log(" data =", data);
+f(data);
+}
+});
+}
